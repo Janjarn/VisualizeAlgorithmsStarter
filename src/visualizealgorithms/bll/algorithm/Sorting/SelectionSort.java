f96f590a01b34217717a8fd9bd67e4677e8070c0
Janjarn <morgre01@easv365.dk>
2023-03-14 14:01:04 +0100

    y

---
+++ src/visualizealgorithms/bll/algorithm/Sorting/SelectionSort.java
@@ -0,0 +1,16 @@
+package visualizealgorithms.bll.algorithm.Sorting;
+
+import visualizealgorithms.bll.algorithm.AlgorithmType;
+import visualizealgorithms.bll.algorithm.GenericAlgorithm;
+
+public class SelectionSort extends GenericAlgorithm {
+
+    public SelectionSort() {
+        super("SelectionSort", "Simple O(N^2) sorting algorithm" , AlgorithmType.SORTING);
+    }
+
+    @Override
+    public void doWork() {
+        int[]b = (i)
+    }
+}
